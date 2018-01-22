f3ffc744c53ab9d756d7ebc04ede85773cb9a040
lbroudoux <laurent.broudoux@gmail.com>
2018-01-22 18:01:57 +0100

    Adding sources for bluepring demonstration

---
+++ blueprint-demo/greeter-api/src/main/java/com/github/lbroudoux/greeter/GreeterResource.java
@@ -0,0 +1,29 @@
+package com.github.lbroudoux.greeter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ * A JAX-RS resource for exposing REST endpoints for Greeter manipulation
+ */
+@Path("api")
+public class GreeterResource {
+
+    private static Logger log = Logger.getLogger(GreeterResource.class.getName());
+
+    @GET
+  @Path("greeting/{name}")
+  @Produces({"application/json"})
+  public String greet(@Context SecurityContext context, @PathParam("name") String name) {
+    log.log(Level.INFO, "Getting new greet request for " + name);
+    String response = "Hello " + name + "!";
+    log.log(Level.INFO, "Found greeting result " + response);
+    return "{\"response\":\"" + response + "\"}";
+  }
+}
